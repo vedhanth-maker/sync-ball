2964064860cf409a586f1876d8b6600eec4c3e7d
vedhanth <vedhanthr4@gmail.com>
2021-06-02 17:12:33 +0530

    synchronoused

--- sketch.js
+++ sketch.js
@@ -1,29 +1,49 @@
-var ball;
-
+var database;
+var hipnauticBall, position;
 function setup(){
+    database=firebase.database();
+
     createCanvas(500,500);
-    ball = createSprite(250,250,10,10);
-    ball.shapeColor = "red";
+    hipnauticBall = createSprite(250,250,10,10);
+    hipnauticBall.shapeColor = "red";
+    var hipnauticBallposition=database.ref("ball/position");
+    hipnauticBallposition.on("value",readPosition,showError);
 }
 
 function draw(){
     background("white");
+    if(position !==undefined){
+
+    
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writePosition(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writePosition(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writePosition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writePosition(0,+1);
     }
     drawSprites();
 }
+}
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function writePosition(x,y){
+    database.ref("ball/position").set({
+        "x": position.x+x, 
+        "y": position.y+y
+    })
+    
+}
+function readPosition(data){
+    position=data.val();
+    console.log(position.x);
+    hipnauticBall.x = position.x ;
+    hipnauticBall.y = position.y ;
+}
+function showError(){
+    console.log("error in data")
 }
